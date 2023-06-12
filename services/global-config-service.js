8b6efd8562908477ab5cd89594ab74e0a761b319
Md. Imran Hasan <imranhasan@ad.enosisbd.com>
2023-06-12 10:41:19 +0600

    global config update bug fixed

--- services/global-config-service.js
+++ services/global-config-service.js
@@ -1,7 +1,7 @@
 const global_config = require('../models/global-config')
 
 async function getAnnualLeaveCount(){
-    const x = await global_config.findOne({configName: 'yearly-leave'})
+    const x = await global_config.findOne({configName: 'leave-count'})
     return x
 }
 
