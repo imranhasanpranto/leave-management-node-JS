ddf45f7fe2e579f4c099787586cf369ffdd0fe93
Md. Imran Hasan <imranhasan@ad.enosisbd.com>
2023-06-14 10:26:17 +0600

    business logic separated from global config controller to service

--- controllers/global-config-controller.js
+++ controllers/global-config-controller.js
@@ -1,11 +1,10 @@
 const global_config = require('../models/global-config')
-const validator = require('@hapi/joi')
-const {updateUserAnnualLeaveCount} = require('../services/leave-count-service')
+const {addGlobalConfiguration, updateGlobalConfiguration} = require('../services/global-config-service')
 
-const configSchema = validator.object({
-    configName: validator.string().required(),
-    configValue: validator.number().integer().required()
-})
+const {UnAuthorizedAccessError} = require('../exceptions/UnAuthorizedAccessError')
+const {AlreadyExistsError} = require('../exceptions/AlreadyExistsError')
+const {MethodArgumentNotValidError} = require('../exceptions/MethodArgumentNotValidError')
+const {NotFoundError} = require('../exceptions/NotFoundError')
 
 const getByConfigName = async (req, res)=>{
     const name = req.params.name
@@ -18,53 +17,35 @@ const getByConfigName = async (req, res)=>{
 }
 
 const addGlobalConfig = async (req, res)=>{
-    const {userId, role} = req.user
-    if(role ==="Admin"){
-        const globalConfig = await global_config.findOne({configName: req.body.configName})
-        console.log(globalConfig)
-        if(globalConfig){
-            return res.status(409).json({status: false, messsage:'Already exists'})
+    try {
+        await addGlobalConfiguration(req)
+        res.status(200).json({status: true, message: 'global config added!!!'})
+    } catch (error) {
+        if(
+            error instanceof AlreadyExistsError || 
+            error instanceof MethodArgumentNotValidError || 
+            error instanceof UnAuthorizedAccessError
+            ){
+            return res.status(error.httpCode).json({status: false, message: error.message})
         }else{
-            const globalConfig = new global_config({
-                configName: req.body.configName,
-                configValue: req.body.configValue
-            })
-
-            try {
-                const {error} = await configSchema.validateAsync(req.body)
-                if(error){
-                    res.status(400).json({status: false, message: error.details[0].message})
-                }else{
-                    const savedConfig = await globalConfig.save()
-                    res.status(200).json({status: true, message: 'global config added!!!'})
-                }
-            } catch (error) {
-                res.status(500).json({status: false, message: error})
-            }
+            res.status(500).json({status: false, message: "Server error"})
         }
-    }else{
-        res.status(401).json({status: false, messsage:'Unauthorized access'})
     }
 }
 
 const updateGlobalConfig = async (req, res)=>{
-    const {userId, role} = req.user
-    if(role ==="Admin"){
-        let globalConfig = await global_config.findOne({configName: req.body.configName})
-        if(globalConfig){
-            await updateUserAnnualLeaveCount(req.body.configValue, globalConfig.configValue);
-            globalConfig.configValue = req.body.configValue
-            try {
-                const savedConfig = await globalConfig.save()
-                res.status(200).json({status: true, message: 'global config updated!!!'})
-            } catch (error) {
-                res.status(500).json({status: false, message: error})
-            }
+    try {
+        await updateGlobalConfiguration(req)
+        res.status(200).json({status: true, message: req.body.configName+' updated successfully'})
+    } catch (error) {
+        if(
+            error instanceof NotFoundError ||  
+            error instanceof UnAuthorizedAccessError
+            ){
+            return res.status(error.httpCode).json({status: false, message: error.message})
         }else{
-            res.status(404).json({status: false, message: 'Not found'})
+            res.status(500).json({status: false, message: "Server error"})
         }
-    }else{
-        res.status(401).json({status: false, messsage:'Unauthorized access'})
     }
 }
 
--- services/global-config-service.js
+++ services/global-config-service.js
@@ -1,8 +1,71 @@
 const global_config = require('../models/global-config')
+const validator = require('@hapi/joi')
+const {updateUserAnnualLeaveCount} = require('../services/leave-count-service')
+
+const {UnAuthorizedAccessError} = require('../exceptions/UnAuthorizedAccessError')
+const {AlreadyExistsError} = require('../exceptions/AlreadyExistsError')
+const {MethodArgumentNotValidError} = require('../exceptions/MethodArgumentNotValidError')
+const {NotFoundError} = require('../exceptions/NotFoundError')
+
+const configSchema = validator.object({
+    configName: validator.string().required(),
+    configValue: validator.number().integer().required()
+})
 
 async function getAnnualLeaveCount(){
     const x = await global_config.findOne({configName: 'leave-count'})
     return x
 }
 
-module.exports = {getAnnualLeaveCount}
+const addGlobalConfiguration = async (req)=>{
+    const {userId, role} = req.user
+    if(role ==="Admin"){
+        try {
+            console.log(req.body.configName)
+        } catch (error) {
+            console.log(error)
+        }
+        
+        const globalConfig = await global_config.findOne({configName: req.body.configName})
+        if(globalConfig){
+            throw new AlreadyExistsError(globalConfig.configName+" already exists")
+        }else{
+            const globalConfig = new global_config({
+                configName: req.body.configName,
+                configValue: req.body.configValue
+            })
+
+            const {error} = configSchema.validate(req.body)
+            if(error){
+                throw new MethodArgumentNotValidError(error.details[0].message)
+            }else{
+                await globalConfig.save()
+                return true
+            }
+            
+        }
+    }else{
+        throw new UnAuthorizedAccessError("You don't have enough permission to add global configuration.")
+    }
+}
+
+const updateGlobalConfiguration = async (req)=>{
+    const {userId, role} = req.user
+    if(role ==="Admin"){
+        let globalConfig = await global_config.findOne({configName: req.body.configName})
+        if(globalConfig){
+            await updateUserAnnualLeaveCount(req.body.configValue, globalConfig.configValue);
+            globalConfig.configValue = req.body.configValue
+            
+            const savedConfig = await globalConfig.save()
+            return true
+            
+        }else{
+            throw new NotFoundError(req.body.configName + ' doesnot exist')
+        }
+    }else{
+        throw new UnAuthorizedAccessError("You don't have enough permission to add global configuration.")
+    }
+}
+
+module.exports = {getAnnualLeaveCount, addGlobalConfiguration, }
